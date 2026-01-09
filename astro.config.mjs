13dadf4fd6f9a97e68b649d46c3d12fc0fc6c3c6
desert-scorpion-research-facility <desert.scorpion.research.facility@gmail.com>
2026-01-09 00:30:28 -0600

    Add custom domain config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://desert-scorpion-research-facility.github.io',
-    base: '/neon_valley_builders/',
+    site: 'https://neonvalleybuilders.com',
+    // No base needed for custom domain
     output: 'static',
 });
